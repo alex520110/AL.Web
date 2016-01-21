488542551630efd40b9fb2ebfd04a5b1943be0ba
alex <chentao@lu9.com>
2016-01-21 18:22:11 +0800

    111

---
+++ AL.BackEnd/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AL.BackEnd.MvcApplication" Language="C#" %>
---
+++ AL.BackEnd/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AL.BackEnd.Startup))]
+namespace AL.BackEnd
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
